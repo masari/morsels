c23964782ce3268faf14f6c092578cdabfa54885
Test <Test>
2025-09-27 15:16:24 -0700

    Initial Commit

---
+++ MorselsTests/MorselsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorselsTests.swift
+//  MorselsTests
+//
+//  Created by Mark Messer on 9/27/25.
+//
+
+import Testing
+@testable import Morsels
+
+struct MorselsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
